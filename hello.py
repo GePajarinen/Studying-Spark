dc20c90221df2f1bc323957fdabbab129e6de247
Gesiane Pajarinen <gesianef@hotmail.com>
2024-04-15 07:20:46 +0000

    Simple table test

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 from pyspark.sql import *
 
 if __name__ == "__main__":
-    print("OK")
+    
+    spark = SparkSession.builder \
+        .appName("Spark 1") \
+        .master("local[2]") \
+        .getOrCreate()
+    
+    data_list = [("Carla", 28),
+                ("Mara", 33),
+                ("Marcos", 44)]
+    
+    df = spark.createDataFrame(data_list).toDF("name", "age")
+    df.show()
